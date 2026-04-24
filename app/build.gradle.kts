d1f71078257d05996b568844abc26b2b93759729
Daniel Roque <daniel.atdrroque05@gmail.com>
2026-04-24 02:05:27 -0300

    chore: add internet permission and navigation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.coil.compose)
+    implementation(libs.androidx.navigation)
     implementation(libs.squareup.retrofit)
     implementation(libs.squareup.converter)
     implementation(libs.squareup.okhttp3)
