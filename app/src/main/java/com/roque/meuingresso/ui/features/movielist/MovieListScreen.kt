81e3a6e152f3d3d5eb8063482e2ce97fc8f7ee43
Daniel Roque <daniel.atdrroque05@gmail.com>
2026-04-24 01:55:01 -0300

    feat: implement MovieListScreen orchestrator

---
+++ app/src/main/java/com/roque/meuingresso/ui/features/movielist/MovieListScreen.kt
@@ -0,0 +1,56 @@
+package com.roque.meuingresso.ui.features.movielist
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.roque.meuingresso.R
+import com.roque.meuingresso.ui.features.movielist.components.MovieListContent
+import org.koin.androidx.compose.koinViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MovieListScreen(
+    viewModel: MovieListViewModel = koinViewModel()
+) {
+
+    val state by viewModel.uiState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(Unit) {
+        viewModel.handleIntent(MovieListContract.Intent.LoadMovies)
+    }
+
+    LaunchedEffect(Unit) {
+        viewModel.effect.collect { effect ->
+            when (effect) {
+                is MovieListContract.Effect.NavigateToDetails -> {
+                    TODO("Evento de clique para detalhes")
+                }
+                is MovieListContract.Effect.ShowError -> {
+                    TODO("Em caso de erro apresentar snackbar")
+                }
+            }
+        }
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(stringResource(R.string.now_showing)) })
+        }
+    ) { innerPadding ->
+        MovieListContent(
+            state = state,
+            modifier = Modifier.padding(innerPadding),
+            onMovieClick = { movie ->
+                viewModel.handleIntent(MovieListContract.Intent.OnMovieClicked(movie))
+            }
+        )
+    }
+}
